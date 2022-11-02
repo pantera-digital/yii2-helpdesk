e5348c342943f28b15ab96d10a2026b5b5e02171
Alex Kostin <web-mayak@mail.ru>
2022-11-03 00:18:24 +1000

    Рефакторинг DownloadAction, на фронте не работал вообще

--- actions/DownloadAction.php
+++ actions/DownloadAction.php
@@ -3,22 +3,22 @@
 namespace pantera\helpdesk\actions;
 
 use pantera\helpdesk\models\TicketMessages;
-use pantera\helpdesk\models\Tickets;
-use pantera\helpdesk\Service;
 use pantera\media\models\Media;
-use yii\base\Action;
+use yii\web\NotFoundHttpException;
 use Yii;
 
-class DownloadAction extends Action {
-
-    public function run($id) {
-        $model = Media::findOne($id);
-        $ticket = Tickets::findOne($model->model_id);
-        if (!Yii::$app->user->can('admin')) {
-            if ($ticket->user_id !== Yii::$app->user->id) {
-                throw new NotFoundHttpException();
-            }
+class DownloadAction extends \yii\base\Action
+{
+    public function run($id)
+    {
+        if (
+            ($model = Media::findOne($id))
+            && ($ticketMessage = TicketMessages::findOne($model->model_id))
+            && ($ticket = $ticketMessage->ticket)
+            && ($ticket->user_id == Yii::$app->user->id || Yii::$app->user->can('admin'))
+        ) {
+            return Yii::$app->response->sendFile($model->getPath());
         }
-        Yii::$app->response->sendFile($model->getPath());
+        throw new NotFoundHttpException();
     }
 }
